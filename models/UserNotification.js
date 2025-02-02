102f532d5d16afaf14a9c5eb93cc07e227b38ab4
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2025-02-03 00:02:39 +0100

    added UserNotification db

---
+++ models/UserNotification.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const UserNotification = sequelize.define("UserNotification", {
+    StudentId: {
+      type: DataTypes.UUID, // Change to UUID if you're using UUID for Student IDs
+      allowNull: true,
+      references: { model: "Students", key: "id" },
+    },
+    AspirantId: {
+      type: DataTypes.UUID, // Change to UUID if you're using UUID for Aspirant IDs
+      allowNull: true,
+      references: { model: "Aspirants", key: "id" },
+    },
+    NotificationId: {
+      type: DataTypes.UUID, // Match Notification's UUID type
+      allowNull: false,
+      references: { model: "Notifications", key: "id" },
+    },
+    seen: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  });
+
+  return UserNotification;
+};
