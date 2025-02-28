936b8c654da95a20d7fc12c3dfe7efdfffd5a90a
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-02-28 12:38:42 +0100

    Fix: Updated error page render in preventDuplicate admin middlewear and handled unexpected errors to work with new error.ejs

--- middlewares/preventDuplicatAdmin.js
+++ middlewares/preventDuplicatAdmin.js
@@ -3,10 +3,17 @@ const { Admin } = require("../models");
 module.exports = async (req, res, next) => {
   try {
     const adminExists = await Admin.findOne();
-    if (adminExists) return res.render("error.ejs");
+    if (adminExists) return res.render("error.ejs", { error: "" });
 
     return next();
   } catch (error) {
-    console(`ERROR VERIIFYING ADMIN: ${error}`);
+    console.log(`ERROR VERIIFYING ADMIN: ${error}`);
+    return res.status(404).render("error.ejs", {
+      error: {
+        statusCode: 500,
+        title: "Internal Server Error",
+        message: "Something went wrong. Please try again later.",
+      },
+    });
   }
 };
