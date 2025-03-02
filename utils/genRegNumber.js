8f6f1e0e380411e82332749aa45705a34e0d359d
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-03-02 05:33:15 +0100

    Fix: Made genRegNumber utile async to catch errors

--- utils/genRegNumber.js
+++ utils/genRegNumber.js
@@ -1,33 +1,38 @@
 const { Student } = require("../models");
 
 module.exports = async (academicYear) => {
-  let count = 1;
-  const genRegNumber = async () => {
-    const prefix = "DNPSS";
-    const year = academicYear.split("/")[0];
+  try {
+    let count = 1;
+    const genRegNumber = async () => {
+      const prefix = "DNPSS";
+      const year = academicYear.split("/")[0];
 
-    const studentsCount = await Student.count();
-    const studentsNumber = studentsCount + count;
+      const studentsCount = await Student.count();
+      const studentsNumber = studentsCount + count;
 
-    const padRegistrationNumber = (number) => {
-      return String(number).padStart(3, "0");
+      const padRegistrationNumber = (number) => {
+        return String(number).padStart(3, "0");
+      };
+
+      const registrationNumber = `${prefix}-${year}-${padRegistrationNumber(
+        studentsNumber
+      )}`;
+
+      const studentsNumberExsits = await Student.findOne({
+        where: {
+          registrationNumber,
+        },
+      });
+      if (studentsNumberExsits) {
+        count++;
+        return await genRegNumber();
+      }
+
+      return registrationNumber;
     };
-
-    const registrationNumber = `${prefix}-${year}-${padRegistrationNumber(
-      studentsNumber
-    )}`;
-
-    const studentsNumberExsits = await Student.findOne({
-      where: {
-        registrationNumber,
-      },
-    });
-    if (studentsNumberExsits) {
-      count++;
-      return await genRegNumber();
-    }
-
-    return registrationNumber;
-  };
-  return await genRegNumber();
+    return await genRegNumber();
+  } catch (error) {
+    console.log(`ERROR GENERATING REGISTRATION NUMBER: ${error}`);
+    return null;
+  }
 };
