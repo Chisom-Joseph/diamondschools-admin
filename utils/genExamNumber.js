46b5c23493ed4c6af233213d94a8ea77e585f8c9
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-03-02 03:13:15 +0100

    Fix: Made genExamNumber utile async to catch errors

--- utils/genExamNumber.js
+++ utils/genExamNumber.js
@@ -1,32 +1,37 @@
 const { Aspirant } = require("../models");
 
 module.exports = async (academicYear) => {
-  const genExamNumber = async () => {
-    const prefix = "DNPSS";
-    const sufix = "ASP";
-    const year = academicYear.split("/")[0];
+  try {
+    const genExamNumber = async () => {
+      const prefix = "DNPSS";
+      const sufix = "ASP";
+      const year = academicYear.split("/")[0];
 
-    const aspirantsCount = await Aspirant.count();
-    const aspirantsNumber = aspirantsCount + 1;
+      const aspirantsCount = await Aspirant.count();
+      const aspirantsNumber = aspirantsCount + 1;
 
-    const padExamNumber = (number) => {
-      return String(number).padStart(3, "0");
+      const padExamNumber = (number) => {
+        return String(number).padStart(3, "0");
+      };
+
+      const examNumber = `${prefix}-${year}-${padExamNumber(
+        aspirantsNumber
+      )}-${sufix}`;
+
+      const aspirantsNumberExsits = await Aspirant.findOne({
+        where: {
+          examinationNumber: examNumber,
+        },
+      });
+      if (aspirantsNumberExsits) {
+        return await genExamNumber();
+      }
+
+      return examNumber;
     };
-
-    const examNumber = `${prefix}-${year}-${padExamNumber(
-      aspirantsNumber
-    )}-${sufix}`;
-
-    const aspirantsNumberExsits = await Aspirant.findOne({
-      where: {
-        examinationNumber: examNumber,
-      },
-    });
-    if (aspirantsNumberExsits) {
-      return await genExamNumber();
-    }
-
-    return examNumber;
-  };
-  return await genExamNumber();
+    return await genExamNumber();
+  } catch (error) {
+    console.log(`ERROR GENERATING EXAM NUMBER: ${error}`);
+    return null;
+  }
 };
