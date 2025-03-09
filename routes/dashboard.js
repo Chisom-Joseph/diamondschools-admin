c79583ca119786f923b2878018181e8761a55461
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-03-09 06:56:19 +0100

    Fix: Used new error util in academic year page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -421,13 +421,7 @@ router.get("/academic-year", async (req, res) => {
   } catch (error) {
     console.error("ERROR RENDERING ACADEMIC YEAR PAGE");
     console.error(error);
-    return res.status(500).render("error.ejs", {
-      error: {
-        statusCode: 500,
-        title: "Internal Server Error",
-        message: "Something went wrong. Please try again later.",
-      },
-    });
+    require("../utils/showErrorPage")(500, {}, res);
   }
 });
 router.post("/academic-year", require("../controllers/dashboard/academicYear"));
