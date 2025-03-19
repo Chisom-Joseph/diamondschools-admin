f2d019889a7e0148c8b071729360b980c3497f2f
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-03-19 12:47:13 +0100

    Fix: Used new error util in a11 student page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -110,13 +110,7 @@ router.get("/all-students", async (req, res) => {
   } catch (error) {
     console.error("ERROR RENDERING ALL STUDENTS PAGE");
     console.error(error);
-    return res.status(500).render("error.ejs", {
-      error: {
-        statusCode: 500,
-        title: "Internal Server Error",
-        message: "Something went wrong. Please try again later.",
-      },
-    });
+    require("../utils/showErrorPage")(500, {}, res);
   }
 });
 // Add student
