7ac2c7aa0ad0000d88af28a2b05b2311a6253607
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-03-19 12:55:03 +0100

    Fix: Used new error util in add aspirant page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -62,13 +62,7 @@ router.get("/add-aspirant", async (req, res) => {
   } catch (error) {
     console.error("ERROR RENDERING ADD ASPIRANT PAGE");
     console.error(error);
-    return res.status(500).render("error.ejs", {
-      error: {
-        statusCode: 500,
-        title: "Internal Server Error",
-        message: "Something went wrong. Please try again later.",
-      },
-    });
+    require("../utils/showErrorPage")(500, {}, res);
   }
 });
 router.post("/add-aspirant", require("../controllers/dashboard/aspirant"));
