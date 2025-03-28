5f4c947714992afd98d5c5a09620ce3013430b75
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-03-28 21:47:17 +0100

    Feat: Fix typo in examination number reference in block/unblock aspirant message

--- controllers/dashboard/aspirant/blockAspirant.js
+++ controllers/dashboard/aspirant/blockAspirant.js
@@ -26,7 +26,7 @@ module.exports = async (req, res) => {
       status: "success",
       section: "add",
       message: `Aspirant ${
-        req.body.aspirantId ? aspirant.examnitaionNumber : ""
+        req.body.aspirantId ? aspirant.examinationNumber : ""
       } ${newBlockedStatus ? "blocked" : "unblocked"} successfully`,
     });
     req.flash("form", "");
