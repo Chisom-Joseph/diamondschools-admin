592c8ce551b8215ea0121cd83f553c88a1775801
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-03-29 02:33:07 +0100

    Feat: Add authentication token to Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -9,6 +9,8 @@ module.exports = defineConfig({
       BASE_URL: "http://localhost:2395",
       USERNAME: "xukato",
       PASSWORD: "1111111111",
+      A_TOKEN:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjJhMmI5NTgyLTBiMmQtNGU4NS04NjNmLWQ2NjY3NWZmYmIyNiIsImlhdCI6MTc0Mzk0MDE0OSwiZXhwIjoxNzQ0MTk5MzQ5fQ._smF7pTNwZwEqiVBFHG3KuBKqdzs_RiZgShY0OpU-tE",
     },
   },
 });
