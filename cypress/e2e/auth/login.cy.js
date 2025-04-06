cf0d6ebbd6481b3914b39c58a6c79fda0342cc89
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-04-06 01:56:20 +0100

    Feat: Add validation tests for login functionality and update sign-up link in sign-in page

--- cypress/e2e/auth/login.cy.js
+++ cypress/e2e/auth/login.cy.js
@@ -21,10 +21,44 @@ describe("Login Page", () => {
     cy.get("[data-testId=password]").should("have.value", "");
   });
 
+  it("Should show error for invalid email format", () => {
+    cy.get("[data-testId=username]").type("invalidEmailFormat");
+    cy.get("[data-testId=password]").type(Cypress.env("PASSWORD"));
+    cy.get("[data-testId=loginButton]").click();
+    cy.contains("Username, Email, or Password incorrect");
+    cy.get("[data-testId=username]").should("have.value", "invalidEmailFormat");
+    cy.get("[data-testId=password]").should("have.value", "");
+  });
+
+  it("Should show error for invalid password format", () => {
+    cy.get("[data-testId=username]").type(Cypress.env("USERNAME"));
+    cy.get("[data-testId=password]").type("sh");
+    cy.get("[data-testId=loginButton]").click();
+    cy.contains("Username, Email, or Password incorrect");
+    cy.get("[data-testId=username]").should(
+      "have.value",
+      Cypress.env("USERNAME")
+    );
+    cy.get("[data-testId=password]").should("have.value", "");
+  });
+
+  it("Should test Sign-up link", () => {
+    cy.get("[data-testId=signUpLink]").click();
+    cy.url().should("include", "/sign-in");
+  });
+
+  it("Should redirect to login if accessing protected route while unauthenticated", () => {
+    cy.visit(Cypress.env("BASE_URL") + "/dashboard");
+    cy.url().should("include", "/sign-in");
+  });
+
   it("Should allow valid credentials", () => {
     cy.get("[data-testId=username]").type(Cypress.env("USERNAME"));
     cy.get("[data-testId=password]").type(Cypress.env("PASSWORD"));
     cy.get("[data-testId=loginButton]").click();
+    cy.contains("Admin Dashboard").should("exist");
     cy.url().should("include", "/dashboard");
   });
+
+  it("Should logout successfully", () => {});
 });
