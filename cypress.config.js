7be23d589b5d18cb19e122b59aa17605945ca217
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-04-08 10:31:02 +0100

    Feat: Add user and guardian details to Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -11,6 +11,28 @@ module.exports = defineConfig({
       PASSWORD: "1111111111",
       A_TOKEN:
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjJhMmI5NTgyLTBiMmQtNGU4NS04NjNmLWQ2NjY3NWZmYmIyNiIsImlhdCI6MTc0Mzk0MDE0OSwiZXhwIjoxNzQ0MTk5MzQ5fQ._smF7pTNwZwEqiVBFHG3KuBKqdzs_RiZgShY0OpU-tE",
+      USER: {
+        firstName: "Gregory",
+        middleName: "Edwards",
+        lastName: "Landry",
+        academicYear: "2023/2024",
+        country: "AU",
+        religion: "Christianity",
+        dateOfBirth: "2013-12-02",
+        gender: "male",
+        class: "JSS1",
+        address: "Quaerat ea doloremqu",
+        guardian: {
+          firstName: "Leila",
+          middleName: "May",
+          lastName: "Hill",
+          email: "jani@mailinator.com",
+          phoneNumber: "+1(881)409-5554",
+          relationship: "Mother",
+          occupation: "driver",
+          address: "Quod sit molestiae ",
+        },
+      },
     },
   },
 });
